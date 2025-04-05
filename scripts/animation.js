9ed8642e6b16b5723c2d9db18ede7a8aaedf5731
nestah <phestuskitsao254@gmail.com>
2025-04-05 11:17:37 +0300

    first commit

---
+++ scripts/animation.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var options = {
+        strings: [
+            "Web Development",
+            "Data Visualization",
+            "Machine Learning",
+            "Remote Sensing",
+            "Geospatial Analysis"
+        ],
+        typeSpeed: 50, // Speed of typing in milliseconds
+        backSpeed: 25,  // Speed of backspacing in milliseconds
+        backDelay: 1000, // Delay before starting backspacing
+        startDelay: 500, // Delay before typing starts
+        loop: true // Whether to loop the animation
+    };
+
+    var typed = new Typed('#typed-output', options);
+});
